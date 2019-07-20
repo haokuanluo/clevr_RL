ec1007d8f58656f80809a904c185868db012bc6d
root <haokuan@mit.edu>
2019-07-20 19:46:05 +0000

    g

--- test.py
+++ test.py
@@ -1,23 +1,26 @@
 import gym
 import time
+import pickle
 
 env = gym.make('gym_tdw:tdw-v0')
 env.set_observation(True)
-
+print(env.action_space)
+ar = []
 for i in range(100):
-    if i == 0 or i == 100:
-        action = env.sample_random_action()
+    action = env.sample_random_action()
 
-    else:
-        action = {
-            "x": 0,
-            "z": 0
-        }
+   # else:
+   #     action = {
+   #         "x": 0,
+   #         "z": 0
+   #     }
 
     obs, reward, episode_done, _ = env.step(action)
     print(reward)
     print(action)
     print(obs['image_1'].shape)
+    ar.append(obs['image_1'])
+    pickle.dump(ar,open('img.p','wb'))
     if episode_done:
         break
 
