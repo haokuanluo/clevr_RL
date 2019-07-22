3f8c95e652809735023652264d37e4e2358e734f
admin <haokuan@mit.edu>
2019-07-21 22:06:22 -0400

    gg

--- rl.py
+++ rl.py
@@ -10,16 +10,18 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
 import time
+import pickle
 
 #Parameters
-env = gym.make('gym_tdw:tdw-v0')
-#env = env.unwrapped
+env = gym.make('gym_tdw:tdw_puzzle_1-v0')
+env.set_observation(True)
 
 #env.seed(1)
 torch.manual_seed(1)
 
-state_space = env.observation_space.shape[0]
-action_space = env.action_space.n
+C,H,W = 3,480,640
+grid = 20
+action_space = grid*grid
 
 
 #Hyperparameters
@@ -29,15 +31,26 @@ episodes = 20000
 render = False
 eps = np.finfo(np.float32).eps.item()
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
-emb_dim = 32
-action_dim = 2
+rewards = []
+
 class Policy(nn.Module):
     def __init__(self):
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(state_space, emb_dim)
 
-        self.action_mu = nn.Linear(emb_dim, action_dim)
-        self.action_var = nn.Linear(emb_dim, action_dim)
+        self.conv1 = torch.nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        # 4608 input features, 64 output features (see sizing flow below)
+        self.fc1 = torch.nn.Linear(18 * H * W / 4, 64)
+
+        # 64 input features, 10 output features for our 10 defined classes
+        self.fc2 = torch.nn.Linear(64, 32)
+
+
+
+
+
+        self.action_head = nn.Linear(32, action_space)
         self.value_head = nn.Linear(32, 1) # Scalar Value
 
         self.save_actions = []
@@ -45,18 +58,23 @@ class Policy(nn.Module):
         os.makedirs('./AC_CartPole-v0', exist_ok=True)
 
     def forward(self, x):
+        x = x.permute(2,0,1)
+
+        x = F.relu(self.conv1(x))
+
+        # Size changes from (18, 32, 32) to (18, 16, 16)
+        x = self.pool(x)
+
+
+        x = x.view(-1, 18 * H * W / 4)
+
+
         x = F.relu(self.fc1(x))
-        mu = self.action_mu(x)
-        var = self.action_var(x)
-        m = (self.sample_gaussian(mu[0],var[0]),self.sample_gaussian(mu[1],var[1]))
-        #action_score = self.action_head(x)
+        x = F.relu(self.fc2(x))
+        action_score = self.action_head(x)
         state_value = self.value_head(x)
 
-        return m, state_value
-    def sample_gaussian(self,mu,var):
-        std = torch.exp(0.5 * var)
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
+        return F.softmax(action_score, dim=-1), state_value
 
 model = Policy()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -113,16 +131,23 @@ def finish_episode():
     del model.rewards[:]
     del model.save_actions[:]
 
+def transform_action(action):
+    multiplier = 200/grid
+    return {'x':(action//grid)*multiplier-100,'z':(action%grid)*multiplier-100}
 def main():
     running_reward = 10
     live_time = []
     for i_episode in count(episodes):
         state = env.reset()
+        state = state['image_1']
         for t in count():
             action = select_action(state)
-            state, reward, done, info = env.step(action)
+            state, reward, done, info = env.step(transform_action(action))
+            state = state['image_1']
             if render: env.render()
             model.rewards.append(reward)
+            rewards.append(reward)
+
 
             if done or t >= 1000:
                 break
@@ -132,30 +157,9 @@ def main():
         if i_episode % 100 == 0:
             modelPath = './AC_CartPole_Model/ModelTraing'+str(i_episode)+'Times.pkl'
             torch.save(model, modelPath)
+            print(i_episode)
+            pickle.dump(rewards,open('AC_rewards.p','wb'))
         finish_episode()
 
 if __name__ == '__main__':
     main()
-
-
-env.set_observation(True)
-
-for i in range(100):
-    if i == 0 or i == 100:
-        action = env.sample_random_action()
-
-    else:
-        action = {
-            "x": 0,
-            "z": 0
-        }
-
-    obs, reward, episode_done, _ = env.step(action)
-    print(reward)
-    print(action)
-    print(obs['image_1'].shape)
-    if episode_done:
-        break
-
-# Wait until the simulator sends all messages
-time.sleep(20)
