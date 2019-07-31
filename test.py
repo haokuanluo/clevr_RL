ba55159be38f98eec93665ad678059dfcfcd3583
root <root@cleavr.IBM.cloud>
2019-07-31 05:33:58 +0000

    gg

--- test.py
+++ test.py
@@ -1,28 +1,20 @@
-import gym
-import time
-import pickle
-
-env = gym.make('gym_tdw:tdw_puzzle_1-v0')
-env.set_observation(True)
-print(env.action_space)
-ar = []
-for i in range(100):
-    action = env.sample_random_action()
-#    action = {'x':0,'z':0}
-   # else:
-   #     action = {
-   #         "x": 0,
-   #         "z": 0
-   #     }
-
-    obs, reward, episode_done, _ = env.step(action)
-    print(reward)
-    print(action)
-    print(obs['image_1'].shape)
-    ar.append(obs['image_1'])
-    pickle.dump(ar,open('img.p','wb'))
-    if episode_done:
-        break
-
-# Wait until the simulator sends all messages
-time.sleep(20)
+import gym                                           
+import time                                          
+import json                                          
+env = gym.make('gym_tdw:tdw_puzzle_1-v0')            
+env.set_observation(True)                            
+for i in range(10):                                  
+    if i == 0 or i == 35:                            
+        action = {                                   
+            "x": 70,                                 
+            "z": -50                                 
+            }                                            
+    else:                                            
+        action = {                                   
+            "x": 0,                                  
+            "z": 0                                   
+            }                                                                                                                                                                                                                                                            
+    obs, reward, episode_done, _ = env.step(action)  
+    if i == 10:                                      
+        env.reset()                                  
+env.close()
